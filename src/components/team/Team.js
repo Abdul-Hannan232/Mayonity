15d750e4b580bf1d28c830ca4c9d3bb3f070605a
Abdul Hannan <abdulhani232@gmail.com>
2022-04-17 07:12:20 +0500

    updated utpo home team section

--- src/components/team/Team.js
+++ src/components/team/Team.js
@@ -1,39 +1,70 @@
+import { useState, useEffect, useLayoutEffect } from 'react';
 import { Link } from "react-router-dom";
 import SingleTeamMember from "./SingleTeamMember";
 import TeamData from "../../data/team/team-member.json";
 
 export default function Team(props) {
-    const {subTitle, title, para, btnLink, btnText} = props
+    const [teamText, setTeamText] = useState([]);
+    const [teamMates, setTeamMates] = useState([]);
+
+    // const { subTitle, title, para, btnLink, btnText } = props
+
+    useLayoutEffect(() => {
+        const fetchTeam = async () => {
+            const [teamTextResponse] = await (await fetch("https://www.admin.mayonity.com/api/home-team-info?_format=json")).json();
+
+            const teamMatesResponse = await (await fetch("https://www.admin.mayonity.com/api/all-team-members?_format=json")).json();
+
+            // https://www.admin.mayonity.com
+
+            console.log("teamTextResponse : ", teamTextResponse);
+            console.log("teamMatesResponse url img : ", teamMatesResponse[0].field_tm_memberimage);
+
+            setTeamText(teamTextResponse);
+            setTeamMates(teamMatesResponse);
+
+        }
+        fetchTeam();
+
+    }, [])
 
 
-
- 
-    const teamMembers = TeamData.slice(0, 2);
+    const teamMembers = teamMates.slice(0, 2);
     const allTeamMember = teamMembers.map((item, index) => (
-        <SingleTeamMember 
-            key={index} 
-            memberImage={item.memberImage} 
-            memberName={item.memberName} 
-            designation={item.designation} 
-            contactUrl={item.contactUrl} 
-            contactText={item.contactText} 
+        <SingleTeamMember
+            key={index}
+            memberImage={"https://www.admin.mayonity.com"+item.field_tm_memberimage}
+            memberName={item.field_tm_membername}
+            designation={item.field_tm_designation}
+            // contactUrl={item.contactUrl}
+            // contactText={item.contactText}
         />
     ))
- 
-    return(
-        <div className="saasbox-team-area pt-120 pb-120 bg-gradient">            
+
+    return (
+        <div className="saasbox-team-area pt-120 pb-120 bg-gradient">
             <div className="container">
                 <div className="row align-items-center justify-content-between">
                     <div className="col-12 col-sm-9 col-md-7 col-lg-5">
                         <div className="section-heading mb-lg-0">
-                            <h6 className="text-dark">Creative Sailors</h6>
-                            <h2> Our world class experienced team members</h2>
-                            <p className="text-dark">Its crafted with the latest trend of design & coded with all modern approaches.</p>
-                            <Link className="btn btn-light mt-5" to="/team" >
-                                View All Members <i className="ms-3 bi bi-arrow-right" />
-                            </Link>
+                            {teamText.field_ht_subheading ? <h6 className="text-dark"> {teamText?.field_ht_subheading} </h6> : <h6 className="text-dark">Creative Sailors</h6>}
 
-                            <div className="dot-pattern" style={{backgroundImage: `url(/assets/img/core-img/dot.png)`}} />
+                            {teamText.field_ht_mainheading ? <h2> {teamText.field_ht_mainheading} </h2> : <h2> Our world class experienced team members</h2>}
+
+                            {teamText.info ? <p className="text-dark"> {teamText.info} </p> : <p className="text-dark">Its crafted with the latest trend of design & coded with all modern approaches.</p>}
+
+                            {/* //field_ht_btn_text */}
+                            {teamText.field_ht_btn_link ?
+                                <Link className="btn btn-light mt-5" to={teamText.field_ht_btn_link} >
+                                    {teamText.field_ht_btn_text} <i className="ms-3 bi bi-arrow-right" />
+                                </Link> :
+
+                                <Link className="btn btn-light mt-5" to="/team" >
+                                    View All Members <i className="ms-3 bi bi-arrow-right" />
+                                </Link>}
+
+
+                            <div className="dot-pattern" style={{ backgroundImage: `url(/assets/img/core-img/dot.png)` }} />
                         </div>
                     </div>
 
